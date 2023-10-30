d87f82e634d562e3885f35355e86643d796a25dd
nairansantos <144444085+nairansantos@users.noreply.github.com>
2023-10-29 22:22:38 -0300

    Add files via upload
    Nairan bsantos   
    responsavel por implentar dados do tipo string.

---
+++ listaNomes.h
@@ -0,0 +1,59 @@
+#include "listaNomes.h"
+#include <iostream>
+#include <algorithm>
+
+void entradaDeDados()
+{
+  cout << "Quantos nomes na lista: ";
+  int n;
+  cin >> n;
+  cin.ignore();
+
+  for (int i = 0; i < n; i++)
+  {
+    string nome;
+    cout << "Digite o nome " << i + 1 << ": ";
+    getline(cin, nome);
+    lista.push_back(nome);
+  }
+}
+
+void mostraMediana()
+{
+  if (lista.empty())
+  {
+    cout << "A lista de nomes está vazia." << endl;
+  }
+  else
+  {
+    sort(lista.begin(), lista.end());
+    int meio = lista.size() / 2;
+    cout << "Mediana da lista de nomes: " << lista[meio] << endl;
+  }
+}
+
+void mostraMenor()
+{
+  if (lista.empty())
+  {
+    cout << "A lista de nomes está vazia." << endl;
+  }
+  else
+  {
+    sort(lista.begin(), lista.end());
+    cout << "Primeiro nome alfabeticamente: " << lista[0] << endl;
+  }
+}
+
+void mostraMaior()
+{
+  if (lista.empty())
+  {
+    cout << "A lista de nomes está vazia." << endl;
+  }
+  else
+  {
+    sort(lista.begin(), lista.end());
+    cout << "Último nome alfabeticamente: " << lista[lista.size() - 1] << endl;
+  }
+}
